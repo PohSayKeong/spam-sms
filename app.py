57d2b27a334b3ca3d18d7c5f1c83799df7f700af
Poh Say Keong <poh.saykeong@dhs.sg>
2019-03-05 14:16:54 +0800

    Normalize text

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, url_for, request, flash
 import fastText
+import re
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'ed025c9f99a6e3d55185'
+
+def strip_formatting(string):
+    string = string.lower()
+    string = re.sub(r"([.!?,'/()])", r" \1 ", string)
+    return string
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     classifier = fastText.load_model('spam_model.bin')
     if request.method == 'POST':
-        labels = classifier.predict(request.form['input_text'])
+        normalized = strip_formatting(request.form['input_text'])
+        labels = classifier.predict(normalized)
         confidence = float(str(labels[1])[1:-1]) * 100
         if str(labels[0]) == str("('__label__ham',)"):
             flash("Text: " + request.form['input_text'])
